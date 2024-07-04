1cf09d71f188d2312a52b18400b23578cdc11f41
Umut Bilal Okur <okur.umutbilal@gmail.com>
2024-07-04 22:40:40 +0300

    initial commit

---
+++ src/main/java/org/ubo/linonbackend/LinonBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ubo.linonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ubo/linonbackend/LinonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ubo.linonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
